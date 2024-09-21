6bd6a23f7747ffd7d9e65331452b4a0f53a7acf4
Tenille <elmofreak@gmail.com>
2024-09-21 12:50:05 +0800

    adding some functions for getting and posting projects

---
+++ crowdfunding/projects/urls.py
@@ -0,0 +1,9 @@
+# comments when i feel like it
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('projects/', views.ProjectList.as_view()),
+  path('projects/<int:pk>/', views.ProjectDetail.as_view()),
+]

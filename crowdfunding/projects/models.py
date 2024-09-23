e924c66baef2ae9217fe3c983080a2b00d5d296d
Tenille <elmofreak@gmail.com>
2024-09-23 17:53:29 +0800

    add pledge

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -13,3 +13,14 @@ class Project(models.Model):
     project_type = models.TextField(choices=ProjType)
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
